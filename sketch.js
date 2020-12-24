23f84d54b341fef4ffbd00aac5661ad9a0bef051
MightyMusker <74767936+MightyMusker@users.noreply.github.com>
2020-12-24 15:39:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,6 @@ var weight,speed;
 
 function setup() {
   createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
 
   bullet=createSprite(10,200,20,50);
   wall=createSprite(1500,200,thickness,height/2);
